aa5e95ba40584dcf48028797ef4ee49bb21d9e97
NikeshDeveloper3999 <nikeshparte726@gmail.com>
2026-06-04 20:12:22 +0530

    add

--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const userController = require("../controllers/usercontroller");
-const authMiddleware = require("../middleware/authuser");
+const authMiddleware = require("../Middleware/authuser");
 
 router.post("/register", userController.registerUser);
 router.post("/login", userController.loginUser);
